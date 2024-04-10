7d3267146e0272922c74db6b4a305f96a25f09d1
Kate <alekseeva.katusha1@gmail.com>
2024-04-10 11:30:28 +0300

    Initial commit

---
+++ src/components/question.js
@@ -0,0 +1,21 @@
+const question=[
+	{
+		id:0,
+		title:"How long is an IPv6 address?",
+		options:["128 bits","64 bits","128 bytes","32 bits"],
+		ans:0,
+	},
+	{
+		id:1,
+		title:"In CSS, which of these values CANNOT be used with the \"position\" property?",
+		options:["static","relative","cente","absolute"],
+		ans:2,
+	},
+	{
+		id:2,
+		title:"In the server hosting industry IaaS stands for..",
+		options:["Infrastructure as a Service","Internet and a Server","Internet as a Service","Infrastructure as a Server"],
+		ans:0,
+	}
+];
+export default question;
